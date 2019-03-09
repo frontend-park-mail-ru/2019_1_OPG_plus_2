8498036f99c99450df4668af74d7ab186dadd58d
Ivan <ivan.morozov@list.ru>
2019-03-09 17:49:29 +0300

    add user page profile

--- src/pages/profile_page.js
+++ src/pages/profile_page.js
@@ -5,15 +5,14 @@ import Head from '../blocks/html/body/application/container/head/head.js';
 import BackArrow from '../blocks/html/body/application/container/head/back-arrow/back_arrow.js';
 import Content from '../blocks/html/body/application/container/content/content.js';
 import Menu from '../blocks/html/body/application/container/head/menu/menu.js'
+import ProfileCard from '../blocks/html/body/application/container/content/profile-card/profile-card.js';
+import ProfileHead from '../blocks/html/body/application/container/content/profile-card/profile-head/profile-head.js';
+import Name from '../blocks/html/body/application/container/content/profile-card/profile-head/name/name.js';
+import ProfileData from '../blocks/html/body/application/container/content/profile-card/profile-data/profile-data.js';
+import DataItem from '../blocks/html/body/application/container/content/profile-card/profile-data/data-item/data-item.js';
+import Avatar from '../blocks/html/body/application/container/content/profile-card/profile-head/avatar/avatar.js';
 import LogoutIcon from '../blocks/html/body/application/container/head/menu/logout/logout.js';
 import SettingsIcon from '../blocks/html/body/application/container/head/menu/settings/settings.js';
-// import ProfileCard from '../blocks/html/body/application/container/content/profile-card/profile-card.js';
-// import {ProfileHead} from '../blocks/html/body/application/container/content/profile-card/profile-head/profile-head.js';
-// import {Avatar} from '../blocks/html/body/application/container/content/profile-card/profile-head/avatar/avatar.js';
-// import {EditIcon} from '../blocks/html/body/application/container/content/profile-card/profile-head/edit/edit.js';
-// import {Data} from '../blocks/html/body/application/container/content/profile-card/data/data.js';
-// import {Item} from '../blocks/html/body/application/container/content/profile-card/data/item/item.js';
-// import {makeSafe} from '../utils/utils.js';
 
 export default class ProfilePage {
     constructor({
@@ -42,7 +41,7 @@ export default class ProfilePage {
         });
         head.render();
         const headBlock = document.querySelector('.head');
-
+        
         const backArrow = new BackArrow({
             el: headBlock,
             href: '/',
@@ -56,6 +55,62 @@ export default class ProfilePage {
         });
         content.render();
         const contentBlock = document.querySelector('.content.content_theme_profile');
+
+        const profileCard = new ProfileCard({
+            el: contentBlock,
+        });
+        profileCard.render()
+        const profileCardBlock = document.querySelector('.profile-card');
+
+        const profileHead = new ProfileHead({
+            el: profileCardBlock,
+        });
+        profileHead.render();
+        const profileHeadBlock = document.querySelector('.profile-head');
+
+        const avatar = new Avatar({
+            el: profileHeadBlock,
+        });
+        avatar.render();
+
+        const name = new Name({
+            el: profileHeadBlock,
+            name: 'Vasya Pupkin',
+        });
+        name.render();
+
+        const profileData = new ProfileData({
+            el: profileCardBlock,
+        });
+        profileData.render();
+        const profileDataBlock = document.querySelector('.profile-data');
+
+        const titles = {
+            'Score' : 45,
+            'Games played': 60,
+            'Win': 47,
+            'Lose': 13,
+        }
+
+        const modifiers = {
+            'Score' : 'data-item_type_score',
+            'Games played': 'data-item_type_games',
+            'Win': 'data-item_type_win',
+            'Lose': 'data-item_type_lose',
+        }
+
+        Object.entries(titles).forEach((entry) => {
+            const title = entry[0];
+            const data = entry[1];
+
+            const dataItem = new DataItem({
+                el: profileDataBlock,
+                title: title,
+                data: data,
+                modifiers: [modifiers[title]],
+            });
+            dataItem.render();
+        });
 
         const menu = new Menu({
             el: containerBlock,
