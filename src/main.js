08c653b1e4fccfe61333f863ca02e0802793852c
Ivan <ivan.morozov@list.ru>
2019-04-01 02:29:54 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 import 'normalize.css';
 import './scss/style.scss';
 
-// import MainPage from './pages/main_page.js';
-import SignInPage from './pages/signin_page.js';
-import SignUpPage from './pages/signup_page.js';
-import ScoreBoard from './pages/scoreboard_page.js';
-import ProfilePage from './pages/profile_page.js';
-import EditProfilePage from './pages/edit_profile_page.js';
 import Router from './modules/router.js';
-import NotFound from './pages/404.js';
+// import NotFound from './pages/404.js';
 
 import MainPageModel from './app/model/main_page_model';
 import MainPageView from './app/view/main_page_view';
@@ -72,30 +66,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	router.add('/editme', editProfileController);
 
 	router.start();
-
-	// const router = new Router({
-	// 	root: document.getElementById('application'),
-	// });
-	// router.add('/', new MainPage({
-	// 	router: router,
-	// }));
-	// router.add('/signin', new SignInPage({
-	// 	router: router,
-	// }));
-	// router.add('/me', new ProfilePage({
-	// 	router: router,
-	// }));
-	// router.add('/signup', new SignUpPage({
-	// 	router: router,
-	// }));
-	// router.add('/editme', new EditProfilePage({
-	// 	router: router,
-	// }));
-	// router.add('/leaders', new LeaderBoard({
-	// 	router: router,
-	// }));
-
-	// router.add('/not_found', new NotFound());
-
-	// router.start();
 });
