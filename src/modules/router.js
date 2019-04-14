66dd3209048e98278cded528e8f6fa685b9bc479
DanikNik <sway9009>
2019-04-14 14:10:42 +0300

    refresh router bug fix

--- src/modules/router.js
+++ src/modules/router.js
@@ -1,44 +1,43 @@
-import Controller from '../app/controller/controller';
-
 export default class Router {
-	constructor({
-		routes = {},
-		mode = null,
-		root = document.getElementById('application'),
-	} = {}) {
-		this.routes = routes;
-		this.mode = mode == 'history' && !!(history.pushState) ? 'history' : 'hash';
-		this.root = root;
-		window.onpopstate = () => {
-			this.routes[location.pathname].open({root: this.root});
-		};
-	}
+    constructor({
+                    routes = {},
+                    mode = null,
+                    root = document.getElementById('application'),
+                } = {}) {
+        this.routes = routes;
+        this.mode = mode === 'history' && !!(history.pushState) ? 'history' : 'hash';
+        this.root = root;
+        window.onpopstate = () => {
+            this.routes[location.pathname].open({root: this.root});
+        };
+    }
 
-	back() {
-		history.back();
-	}
+    back() {
+        history.back();
+    }
 
-	add({re = '/', handler} = {}) {
-		this.routes[re] = handler;
-	}
+    add({re = '/', handler} = {}) {
+        this.routes[re] = handler;
+    }
 
-	navigate({ path = '/', data = {} } = {}) {
-		if(this.mode === 'history') {
-		  history.pushState(null, null, path);
+    navigate({path = '/', data = {}} = {}) {
+        if (this.mode === 'history') {
+            if (window.location.pathname !== path) {
+                history.pushState(null, null, path);
+            }
+            if (!this.routes[path]) {
+                return;
+            }
 
-		  if (!this.routes[path]) {
-			  return;
-		  }
+            this.routes[path].open({root: this.root, data: data});
+        } else {
+            window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
+        }
+    }
 
-		  this.routes[path].open({root: this.root, data: data});
-		} else {
-		  window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
-		}
-	}
-
-	start() {
-		if(this.mode === 'history') {
-		  this.navigate({ path: location.pathname });
-		}
-	}
+    start() {
+        if (this.mode === 'history') {
+            this.navigate({path: location.pathname});
+        }
+    }
 }
