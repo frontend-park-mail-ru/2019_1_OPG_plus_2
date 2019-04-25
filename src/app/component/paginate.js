fc4a03b42435e44f4f780566bd2ac2465efb31af
Ivan <ivan.morozov@list.ru>
2019-04-25 08:39:38 +0300

    refactoring

--- src/app/component/paginate.js
+++ src/app/component/paginate.js
@@ -4,14 +4,39 @@ import { genericBeforeEnd } from '../../modules/helpers.js';
 import Component from './component';
 
 export default class Paginate extends Component {
+	constructor({
+		callback = () => null,
+		onNextPage = () => null,
+		onPrevPage = () => null,
+	}) {
+		super({callback});
+		this._onNextPage = onNextPage;
+		this._onPrevPage = onPrevPage;
+
+		this.onNextPage = this.onNextPage.bind(this);
+		this.onPrevPage = this.onPrevPage.bind(this);
+	}
+
 	_createEventListeners() {
 		super._createEventListeners();
-		this._root.addEventListener('click', this._callback, true);
+		this._root.querySelector('.pages__next').addEventListener('click', this.onNextPage, true);
+		this._root.querySelector('.pages__back').addEventListener('click', this.onPrevPage, true);
 	}
 
 	_removeEventListener() {
 		super._removeEventListeners();
-		this._root.removeEventListener('click', this._callback, true);
+		this._root.querySelector('.pages__next').removeEventListener('click', this.onNextPage, true);
+		this._root.querySelector('.pages__back').removeEventListener('click', this.onPrevPage, true);
+	}
+
+	onNextPage(event) {
+		const pageNum = parseInt(this._root.querySelector('.pages__num').textContent);
+		this._onNextPage(event, pageNum);
+	}
+
+	onPrevPage(event) {
+		const pageNum = parseInt(this._root.querySelector('.pages__num').textContent);
+		this._onPrevPage(event, pageNum);
 	}
 
 	_render(data) {
